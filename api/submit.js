e3dfad31271a1c8f8ddcd74440aa88c380677fe8
Subigya <109254269+gsubigya@users.noreply.github.com>
2026-08-28 21:46:54 +0545

    Change allowedOrigin for request validation
    Updated allowedOrigin to match the new website URL.

--- api/submit.js
+++ api/submit.js
@@ -16,7 +16,7 @@ export default async function handler(req, res) {
   }
 
   // Check that the request came from your website
-  const allowedOrigin = "https://YOUR-USERNAME.github.io";
+  const allowedOrigin = " https://gsubigya.github.io/telegram-bot";
 
   const origin = req.headers.origin;
 
